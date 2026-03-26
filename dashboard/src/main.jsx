f8fe10765bae295a17409baf215dad04271aaa2b
Joel Christner <joel.christner@gmail.com>
2026-03-25 23:39:07 -0700

    Add Docker context ignores and avoid duplicate dev requests

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -6,11 +6,9 @@ import { AppProvider } from './context/AppContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </BrowserRouter>,
 )
